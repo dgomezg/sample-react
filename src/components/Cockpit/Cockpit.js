406d39c2e4bbb091d49bd2df0fa86589b97a78fa
David Gomez G <dgomezg@gmail.com>
2019-09-04 11:07:51 +0200

    controlling the useEffect() Behaviour on functional components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -7,9 +7,33 @@ const Cockpit = (props) => {
     // every render cycle 
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
-        //Http request...
+        //Http request... (mocking an http request that takes 1 second)
+        setTimeout(() => {
+            alert('Save data to cloud!')
+        }, 1000 )
+    }, 
+    //The second argum ent for useEffect is an array that points to all 
+    // the variables and data that is used on the effect. 
+    //[props.persons]);
 
-    });
+    //if the effect should be used for multiple fields, those can be added 
+    // to the array
+   //[props.persons, props.some-other-value]);
+     
+    //if we pass an empty array as 2nd argument, we instruct react to 
+    // that the effect has no dependencies and should be run whenever
+    // one of the dependencies changes.
+    // In this case, as Cockpit has no dependenices, an empty array will 
+    // make run only the first time (the default)
+    []);
+
+    //If you have different effects that depend on different data, 
+    // different use Effects can be used linking to different data 
+    // (using for that the second argument.)
+    //useEffect(() => {}, [props.someValue])
+    //useEffect(() => {}, [props.somothervalue])
+
+
 
     const assignedClasses = [];
     let btnClass = '';
