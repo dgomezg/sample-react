d293d759a07249607681de25949fb6d6f563a359
David Gomez G <dgomezg@gmail.com>
2019-09-04 10:07:07 +0200

    Component Updating Lifecycle (for props changes)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Person from './Person/Person';
 
-const persons = (props) => {
-    console.log('[Persons.js] rendering...')
-    return props.persons.map((person, index) => {
-        return (
-          <Person
-            key={person.id}
-            clicked={() => props.clicked(index)}
-            changed={(event) => props.changed(event, person.id)}
-            name={person.name} 
-            age={person.age}
-          />
-        );
-      });
-    };
+class Persons extends Component {
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state;
+    // }
 
-export default persons;
+    componentWillReceiveProps(props) {
+        console.log('[Persons.js] componentWillReceiveProps', props);
+    }
+    
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        //Usually it could be the new scroll coordinates or something like that
+        return { message: 'Snapshot!' }; 
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate'); 
+        console.log(snapshot);
+    }
+
+    render() {
+        console.log('[Persons.js] rendering...')
+        return this.props.persons.map((person, index) => {
+            return (
+              <Person
+                key={person.id}
+                clicked={() => this.props.clicked(index)}
+                changed={(event) => this.props.changed(event, person.id)}
+                name={person.name} 
+                age={person.age}
+              />
+            );
+          });
+        };
+    }
+
+export default Persons;
