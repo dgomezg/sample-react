f06c306935dcb4b717912f0bff9a715741c38f0b
David Gomez G <dgomezg@gmail.com>
2019-09-05 16:36:59 +0200

    Using Higer order components to overcome the limitation of having a root element for component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import classes from './Person.css'
+import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-//            <div className={classes.Person}>
-                [
-                <p key="i1" onClick={this.props.clicked}>
+            <Aux>
+                <p  onClick={this.props.clicked}>
                     I'm {this.props.name} and I am {this.props.age} years old!
-                </p>,
-                <p key="i2">{this.props.children}</p>,
-                <input key="i3"
+                </p>
+                <p>{this.props.children}</p> 
+                <input
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name}/>
-                ]
-//            </div>
+                
+            </Aux>
         )
     }
 }
---
+++ src/hoc/Aux.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+//Defines an empty auxiliary wrapper 
+//that simply renders the contained elements (children)
+const aux = props => props.children;
+
+export default aux;
