8a646701c82cbfc67d17863d387a4097efe854d2
David Gomez G <dgomezg@gmail.com>
2019-09-14 11:34:50 +0200

    Making a Post Selectable

--- 02-http-requests/src/components/FullPost/FullPost.js
+++ 02-http-requests/src/components/FullPost/FullPost.js
@@ -5,18 +5,20 @@ import './FullPost.css';
 class FullPost extends Component {
 
   render() {
-    let post = <p>Please select a Post!</p>;
-    post = (
-      <div className="FullPost">
-        <h1>title</h1>
-        <p>Content</p>
-        <div className="Edit">
-          <button className="Delete">
-            Delete
-          </button>
+    let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = (
+        <div className="FullPost">
+          <h1>title</h1>
+          <p>Content</p>
+          <div className="Edit">
+            <button className="Delete">
+              Delete
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      );  
+    }
     return post;
   }
 }
--- 02-http-requests/src/containers/Blog/Blog.js
+++ 02-http-requests/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts : []
+    posts : [],
+    selectedPostId : null
   }
 
   componentDidMount() {
@@ -25,10 +26,16 @@ class Blog extends Component {
       })
   }
 
+  postSelectedHandler = id => {
+    this.setState({selectedPostId: id});
+  }
 
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key= {post.id} title={post.title} author={post.author}/>
+      return <Post key= {post.id} 
+            title={post.title} 
+            author={post.author}
+            clicked={ () => this.postSelectedHandler(post.id)}/>
     })
     return (
       <div>
@@ -36,7 +43,7 @@ class Blog extends Component {
           {posts}
         </section>
         <section>
-          <FullPost/>
+          <FullPost id={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost />
