255f142485453ad2fbe2b908fc4315600f454a81
David Gomez G <dgomezg@gmail.com>
2019-09-14 11:08:29 +0200

    Reset NewPost to init state

--- 02-http-requests/src/components/NewPost/NewPost.js
+++ 02-http-requests/src/components/NewPost/NewPost.js
@@ -1,44 +1,23 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 import './NewPost.css';
 
 class NewPost extends Component {
-  state = {
-    title: '',
-    body: '',
-    author: 'Max'
-  };
-
-  postDataHandler = () => {
-    axios.post('/posts', this.state).then(res => console.log(res));
-  };
 
   render() {
     return (
       <div className="NewPost">
         <h1>Add a Post</h1>
         <label>Title</label>
-        <input
-          type="text"
-          value={this.state.title}
-          onChange={event => this.setState({ title: event.target.value })}
-        />
+        <input type="text"/>
         <label>Content</label>
-        <textarea
-          rows="4"
-          value={this.state.body}
-          onChange={event => this.setState({ body: event.target.value })}
-        />
+        <textarea rows="4"/>
         <label>Author</label>
-        <select
-          value={this.state.author}
-          onChange={event => this.setState({ author: event.target.value })}
-        >
+        <select>
           <option value="Max">Max</option>
           <option value="Manu">Manu</option>
         </select>
-        <button onClick={this.postDataHandler}>Add Post</button>
+        <button>Add Post</button>
       </div>
     );
   }
