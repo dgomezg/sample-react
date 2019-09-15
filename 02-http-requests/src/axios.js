4fdf46bdc0272d4a0ef7b0bbaa87073c810773f9
David Gomez G <dgomezg@gmail.com>
2019-09-15 12:18:05 +0200

    Setting Default Global configuration for axios

--- 02-http-requests/src/axios.js
+++ 02-http-requests/src/axios.js
@@ -7,13 +7,8 @@ Define default baseURL in index.js for common requests
 and only import instance in components that make
 requests to unique URL.
 */
-const instance = axios.create({
-  baseURL: 'http://jsonplaceholder.typicode.com'
-});
 
 // Override default configurations
-instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
 
 // instance.interceptors.request...
-
 export default instance;
--- 02-http-requests/src/index.js
+++ 02-http-requests/src/index.js
@@ -5,11 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
-/* 
-Default Global Configuration 
+/*
+  Default global configuration
 */
-
-axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com/';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
