c6136dec85a898e2869b97517416646d0cb45b56
David Gomez G <dgomezg@gmail.com>
2019-09-15 14:24:43 +0200

    Added 03-routing module from 02-http-request

---
+++ 03-routing/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+/*
+  Default global configuration
+*/
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com/';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+/* 
+Interceptors: global error handlers
+*/
+//interceptors for the request.
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    //Edit request config before returning it
+    return request;
+  },
+  //the 2nd function is the handler for errors IN SENDING THE REQUEST
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  });
+
+  //Interceptors for the response.
+axios.interceptors.response.use(response => {
+  console.log(response);
+  //Edit request config before returning it
+  return response;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }); 
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
