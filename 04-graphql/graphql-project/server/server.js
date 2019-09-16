7cfbb16960e2fe79086e8085ad694032d6a0b918
David Gomez G <dgomezg@gmail.com>
2019-09-16 17:38:32 +0200

    Create an Apollo Server with a grapqhl query

--- 04-graphql/graphql-project/server/server.js
+++ 04-graphql/graphql-project/server/server.js
@@ -1,12 +1,55 @@
 const express = require('express')
-const PORT = 3500
+const { ApolloServer } = require('apollo-server-express')
+
+const authors = [
+    {
+        id: "1",
+        info: {
+            name: "Joe Kelly",
+            age: 32,
+            gender: "M"
+        }
+    },
+    {
+        id: "2",
+        info: {
+            name: "Mary Jane",
+            age: 27,
+            gender: "F"
+        }
+    }
+]
+
+const typeDefs = `
+    type Author {
+        id: ID!
+        info: Person
+    }
+    type Person {
+        name: String!
+        age: Int!
+        gender: String
+    }
+    type Query {
+        getAuthors: [Author],
+    }
+`
+
+const resolvers = {
+    Query: {
+        getAuthors: () => authors
+    }
+}
+
+const PORT = 3600
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 
-app.use('/graphql', (req, res) => {
-    res.send('Welcome to our authors app')
-});
-
+server.applyMiddleware({
+    app, path: '/graphql'
+})
+ 
 app.listen(PORT, () => {
     console.log('Server running on port: ', PORT);
 });
