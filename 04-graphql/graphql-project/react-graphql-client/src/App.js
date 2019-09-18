df14909e2c2edd9d8812bc3fa017af1116683ddf
David Gomez G <dgomezg@gmail.com>
2019-09-18 12:47:43 +0200

    Added material-ui

--- 04-graphql/graphql-project/react-graphql-client/src/App.js
+++ 04-graphql/graphql-project/react-graphql-client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import AppBar from "material-ui/AppBar";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <p>AppBar</p>
-        <p>BooksComponent</p>
+        <MuiThemeProvider>
+          <AppBar title="GraphQL React" />
+          <p>BooksComponent</p>
+        </MuiThemeProvider>
     </div>
   );
 }
