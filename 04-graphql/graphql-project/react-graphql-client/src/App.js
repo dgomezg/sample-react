4080d12a94ed7b6dcd744cf09281b51f15d3c226
David Gomez G <dgomezg@gmail.com>
2019-09-18 13:00:55 +0200

    Configured Apollo client in App.js

--- 04-graphql/graphql-project/react-graphql-client/src/App.js
+++ 04-graphql/graphql-project/react-graphql-client/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import AppBar from "material-ui/AppBar";
-import Books from './components/Books/Books';
+//import Books from './components/Books/Books';
+import BooksComponentWithData from './components/Books/Books';
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
 
 import './App.css';
 
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://liferay-gs-ci:8091/o/graphql'}),
+  cache: new InMemoryCache()
+});
 function App() {
   return (
     <div className="App">
         <MuiThemeProvider>
-          <AppBar title="GraphQL React" />
-          <Books/>
+          <ApolloProvider client={client}>
+            <AppBar title="GraphQL React" />
+            <BooksComponentWithData/>
+          </ApolloProvider>
         </MuiThemeProvider>
     </div>
   );
