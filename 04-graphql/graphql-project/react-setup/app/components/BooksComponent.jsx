33660e97161576315208a4e7dd261d2213d75867
David Gomez G <dgomezg@gmail.com>
2019-09-18 11:55:19 +0200

    Added boilerplate react-setup client project

---
+++ 04-graphql/graphql-project/react-setup/app/components/BooksComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class BooksComponent extends Component {
+  constructor (props){
+    super(props);
+  }
+
+  render () {
+    return <div> Books Component </div>;
+  }
+}
