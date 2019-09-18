05d58a0f512b7ba3e721ac26fd88e3c79fdd3994
David Gomez G <dgomezg@gmail.com>
2019-09-18 12:12:00 +0200

    Replaced resolvers with Book resolvers

--- 04-graphql/graphql-project/prisma-books/src/index.js
+++ 04-graphql/graphql-project/prisma-books/src/index.js
@@ -3,32 +3,35 @@ const { prisma } = require('./generated/prisma-client')
 
 const resolvers = {
   Query: {
-    feed: (parent, args, context) => {
-      return context.prisma.posts({ where: { published: true } })
-    },
-    drafts: (parent, args, context) => {
-      return context.prisma.posts({ where: { published: false } })
-    },
-    post: (parent, { id }, context) => {
-      return context.prisma.post({ id })
-    },
+    fetchAllBooks: (parent, args, ctx) => {
+      return ctx.prisma.books({});
+    }
+    // feed: (parent, args, context) => {
+    //   return context.prisma.posts({ where: { published: true } })
+    // },
+    // drafts: (parent, args, context) => {
+    //   return context.prisma.posts({ where: { published: false } })
+    // },
+    // post: (parent, { id }, context) => {
+    //   return context.prisma.post({ id })
+    // },
   },
-  Mutation: {
-    createDraft(parent, { title, content }, context) {
-      return context.prisma.createPost({
-        title,
-        content,
-      })
-    },
-    deletePost(parent, { id }, context) {
-      return context.prisma.deletePost({ id })
-    },
-    publish(parent, { id }, context) {
-      return context.prisma.updatePost({
-        where: { id },
-        data: { published: true },
-      })
-    },
+  Mutation: { 
+    // createDraft(parent, { title, content }, context) {
+    //   return context.prisma.createPost({
+    //     title,
+    //     content,
+    //   })
+    // },
+    // deletePost(parent, { id }, context) {
+    //   return context.prisma.deletePost({ id })
+    // },
+    // publish(parent, { id }, context) {
+    //   return context.prisma.updatePost({
+    //     where: { id },
+    //     data: { published: true },
+    //   })
+    // },
   },
 }
 
