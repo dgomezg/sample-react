a26b009743738a5bdfe0db68b655bbb8ead36bcf
David Gomez G <dgomezg@gmail.com>
2019-09-20 10:34:39 +0200

    Initial draft structure for page

--- showroom-demo/src/App.js
+++ showroom-demo/src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>Header-banner</div>
+          <div>
+            <h2>Choose your Travel</h2>
+            <ul>
+              <li>Travel 1</li>
+              <li>Travel 2</li>
+              <li>Travel 3</li>
+            </ul>
+          </div>
+
+          <div>Travel detail</div>
       </header>
     </div>
   );
