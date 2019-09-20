b13d5a9e4445e3d5f5e28f0b933b7d1c68ce4657
David Gomez G <dgomezg@gmail.com>
2019-09-20 10:32:37 +0200

    Added images to carousel component test

--- 05-component-test/carousel/src/App.js
+++ 05-component-test/carousel/src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AwesomeSlider>
+          <div data-src={process.env.PUBLIC_URL + '/img/screen1.png'}></div>
+          <div data-src={process.env.PUBLIC_URL + '/img/Screen2.png'}></div>
+          <div data-src={process.env.PUBLIC_URL + '/img/Screen3.png'}></div>
+          <div data-src={process.env.PUBLIC_URL + '/img/Screen4.png'}></div>
+        </AwesomeSlider>
       </header>
     </div>
   );
