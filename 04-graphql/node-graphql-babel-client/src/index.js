8e2d3abc32f422b3dc1a4389458138a5b085f2d3
David Gomez G <dgomezg@gmail.com>
2019-10-01 19:32:29 +0200

    example querying github API

--- 04-graphql/node-graphql-babel-client/src/index.js
+++ 04-graphql/node-graphql-babel-client/src/index.js
@@ -1,13 +1,31 @@
+import 'cross-fetch/polyfill';
+import ApolloClient, { gql } from 'apollo-boost';
 import 'dotenv/config';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+const GET_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
 
-console.log(user);
 
-console.log(process.env.SOME_ENV_VARIABLE);
+client
+  .query({
+    query: GET_ORGANIZATION,
+  })
+  .then(console.log);
+
